33038f17129c60df5aa16e6ba633ae2306ae7642
Swasthika-P-S <162822982+Swasthika-P-S@users.noreply.github.com>
2026-01-16 15:40:07 +0530

    Delete backend directory

--- backend/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client["lexfix"]
-users_collection = db["users"]
--- backend/users.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-users = {
-    "admin@gmail.com": generate_password_hash("admin123")
-}
